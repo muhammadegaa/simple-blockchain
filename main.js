0b9fde7956219dba9f01dd99057c2fa23f28c98b
MOHAMMAD EGA S <mohammad.ega@ibm.com>
2021-01-14 16:32:46 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,41 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block{
+    constructor(index, timestamp, data, previousHash = ''){
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+    }
+
+    calculateHash(){
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+}
+
+class Blockchain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock(){
+        return new Block(0, "01/01/2021", "Genesis block", "0");
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
+
+let eCoin = new Blockchain();
+eCoin.addBlock(new Block(1, "12/01/2021", { amount: 3 }));
+eCoin.addBlock(new Block(2, "14/01/2021", { amount: 15 }));
+
+console.log(JSON.stringify(eCoin, null, 4));
